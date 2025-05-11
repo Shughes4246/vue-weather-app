fbd568acd9f98194bdb7c19ab5bab47e766efb4e
Shughes4246 <110349148+Shughes4246@users.noreply.github.com.>
2025-05-11 18:33:56 +0100

    Build site and configure for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vue-weather-app/',
+  build: {
+    outDir: 'docs'
+  },
   plugins: [
     vue(),
     tailwindcss(),
